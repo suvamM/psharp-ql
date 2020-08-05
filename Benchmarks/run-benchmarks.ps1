364e610147b8007083c59d867fd6e7ad124c0208
Suvam Mukherjee <t-sumukh@microsoft.com>
2020-08-05 18:07:26 +0530

    adding more logic to run-benchmarks

--- Benchmarks/run-benchmarks.ps1
+++ Benchmarks/run-benchmarks.ps1
@@ -1,19 +1,44 @@
 param(
     [string]$dotnet="dotnet",
     [ValidateSet("Debug","Release")]
-    [string]$configuration="Release"
+    [string]$configuration="Release",
+    [string]$mode="bugfinding",
+    [string]$benchmarks="all",
+    [int]$bx = 100
 )
 
 Import-Module $PSScriptRoot\..\Scripts\powershell\common.psm1
 
 Write-Comment -prefix "." -text "Running the P# reinforcement-learning benchmarks" -color "yellow"
 
-$experiments = "$PSScriptRoot/Experiments"
-Get-ChildItem $experiments -Filter *.test.json |
-Foreach-Object {
-    # $content = Get-Content $_.FullName
-    Write-Comment -prefix "..." -text "Running experiment $_" -color "yellow"
-    & $dotnet $PSScriptRoot/bin/netcoreapp3.1/EvaluationDriver.dll $_
+if ($mode -eq "bugfinding") {
+    Write-Comment -prefix ".." -text "Running in mode $mode" -color "yellow"
+    $experiments = "$PSScriptRoot/Bugfinding"
+    Get-ChildItem $experiments -Filter *.test.json |
+    Foreach-Object {
+        Write-Comment -prefix "..." -text "Running experiment $_" -color "yellow"
+        & $dotnet $PSScriptRoot/bin/netcoreapp3.1/EvaluationDriver.dll $_
+    }
+}
+
+elseif ($mode -eq "datanotdet") {
+    Write-Comment -prefix ".." -text "Running in mode $mode" -color "yellow"
+}
+
+elseif ($mode -eq "statehash") {
+    Write-Comment -prefix ".." -text "Running in mode $mode" -color "yellow"
+}
+
+elseif ($mode -eq "perf") {
+    Write-Comment -prefix ".." -text "Running in mode $mode" -color "yellow"
+}
+
+elseif ($mode -eq "state-coverage") {
+    Write-Comment -prefix ".." -text "Running in mode $mode" -color "yellow"
+}
+
+else {
+    Write-Comment -prefix ".." -text "Bad mode ($mode) specified" -color "red"
 }
 
 Write-Comment -prefix "." -text "Successfully run the P# reinforcement-learning benchmarks" -color "green"
